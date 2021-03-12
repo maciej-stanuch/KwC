fa7461af596da3767cd230b7dc06855a8c455757
Maciej Stanuch <maciejstanuch.ms@gmail.com>
2021-03-12 19:08:40 +0100

    Add package for CLI tool creation

---
+++ bin/commands/about.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const chalk = require('chalk');
+
+module.exports = () => {
+    console.log(chalk.yellowBright(`KwC project is flexible and robust penetration testing tool for professionals.`))
+};
--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    ports: [443, 22],
-    methods: ['GET'],
-    addressBasePath: './db.csv',
-    sites: ['moja.pg.edu.pl', 'google.com', 'wp.pl']
-}
